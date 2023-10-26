2b260433f976a257a0ff09e217019e3119a80eb0
Miguel Carhuas <mikel_ct@hotmail.com>
2023-10-25 22:57:38 -0500

    add Student classes n tier

---
+++ microservice-student/src/main/java/com/microservice/student/controller/StudentController.java
@@ -0,0 +1,31 @@
+package com.microservice.student.controller;
+
+import com.microservice.student.entities.Student;
+import com.microservice.student.service.IStudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/student")
+public class StudentController {
+
+    @Autowired
+    private IStudentService studentService;
+
+    @PostMapping("/create")
+    public void saveStudent(@RequestBody Student student) {
+        studentService.save(student);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<?> findAllStudent() {
+        return ResponseEntity.ok(studentService.findAll());
+    }
+
+    @GetMapping("/search/{id}")
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        return ResponseEntity.ok(studentService.findById(id));
+
+    }
+}
